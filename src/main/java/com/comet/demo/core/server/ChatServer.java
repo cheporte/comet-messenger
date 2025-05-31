7a7e95c2a931a10160946e330810a5590fdb3c35
cherry.sh <cheporte.dev@gmail.com>
2025-05-31 11:35:32 +0000

    Minor warnings fixed

--- src/main/java/com/comet/demo/core/server/ChatServer.java
+++ src/main/java/com/comet/demo/core/server/ChatServer.java
@@ -1,7 +1,6 @@
 package com.comet.demo.core.server;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,8 @@ module com.comet.demo {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires java.sql;
+    requires transitive java.sql;
+    requires transitive javafx.graphics;
 
     opens com.comet.controller to javafx.fxml;
 
